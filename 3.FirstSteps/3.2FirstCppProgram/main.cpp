05777c9738abdb524f488e1b42ce8bd32f207fc2
Luke van der Stokker <lukevanderstokker@gmail.com>
2023-08-04 18:19:28 +0200

    refactor and print name 10 times

--- 3.FirstSteps/3.2FirstCppProgram/main.cpp
+++ 3.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
-int main(){
-
+// Entry point to any C++ function
+int main()
+{
     std::cout << "Number1" << std::endl;
     std::cout << "Number2" << std::endl;
     std::cout << "Number3" << std::endl;
 
+    // Course: Print name 10 times
+    std::cout << "Luke" << std::endl;
+    std::cout << "Luke" << std::endl;
+    std::cout << "Luke" << std::endl;
+    std::cout << "Luke" << std::endl;
+    std::cout << "Luke" << std::endl;
+    std::cout << "Luke" << std::endl;
+    std::cout << "Luke" << std::endl;
+    std::cout << "Luke" << std::endl;
+    std::cout << "Luke" << std::endl;
+    std::cout << "Luke" << std::endl;
+
+    // signifies to the OS that the program exited successfully.
     return 0;
 }
