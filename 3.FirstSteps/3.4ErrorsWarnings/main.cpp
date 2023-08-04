db476771b9ed82c4efee09c523f669c9ea5a1937
Luke van der Stokker <lukevanderstokker@gmail.com>
2023-08-04 18:39:23 +0200

    refactor and comments in 3.4

--- 3.FirstSteps/3.4ErrorsWarnings/main.cpp
+++ 3.FirstSteps/3.4ErrorsWarnings/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 
-int main(){
-   
-   //Compile time error
-   std::cout << "Hello Word!" << std::endl;
+int main()
+{
+    // Compile time error (missing semi-colon will simulate this)
+    std::cout << "Hello Word!" << std::endl;
 
-   //Run time error
-   int value = 7/0;
-   std::cout << "value : " << value << std::endl;
+    // Run time error - program crashes
+    // Funny enough the division itself will not be a problem, it is when we
+    // try capture the result of the division
+    int value = 7 / 0; // Some compilers will warn you about this
+    /*
+        - GCC and Clang just warns you and will let this run
+        - MSVC will not build
+    */
+    // Did not print, but did not see any puke either
+    std::cout << "value : " << value << std::endl;
 
     return 0;
 }
