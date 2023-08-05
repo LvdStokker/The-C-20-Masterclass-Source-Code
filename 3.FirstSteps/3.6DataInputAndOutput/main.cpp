a2a99044d3af30d6edc73a50f3798db935f303a7
Luke van der Stokker <lukevanderstokker@gmail.com>
2023-08-05 11:29:29 +0200

    refactor and assignment 1

--- 3.FirstSteps/3.6DataInputAndOutput/main.cpp
+++ 3.FirstSteps/3.6DataInputAndOutput/main.cpp
@@ -1,51 +1,47 @@
 #include <iostream>
 #include <string>
 
-int main(){
-   //Printing data
-   /*
-   std::cout << "Hello C++20 " << std::endl;
+int main()
+{
+    // Printing data
+    // << going from "my data" to cout where cout dumps to terminal
+    /*
+    std::cout << "Hello C++20 " << std::endl;
 
-   int age{21};
-   std::cout << "Age : " << age << std::endl;
+    int age{21};
+    std::cout << "Age : " << age << std::endl;
 
-   std::cerr << "Error message : Something is wrong" << std::endl;
-   std::clog << "Log message : Something happened " << std::endl; 
-
-   */
-
-   //Data input
-
-   /*
-   int age1;
-   std::string name;
-
-   std::cout << "Please type your name and age : " << std::endl;
-
-   //std::cin >> name;
-   //std::cin >> age1;
-
-   std::cin >> name >> age1;
-
-   std::cout << "Hello " << name << " you are " << age1 << " years old!" << std::endl;
-
+    std::cerr << "Error message : Something is wrong" << std::endl;
+    std::clog << "Log message : Something happened " << std::endl;
     */
 
-    //Data with spaces
+    // Data input - no spaces
+    /*
+    int age1;
+    std::string name;
+
+    std::cout << "Please type your name and age : " << std::endl;
+
+    // std::cin >> name;
+    // std::cin >> age1;
+    >> going from cin (terminal) to my_var
+    std::cin >> name >> age1;
+
+    std::cout << "Hello " << name << " you are " << age1 << " years old!" << std::endl;
+    */
+
+    // Data input - with spaces
 
     std::string full_name;
     int age3;
 
     std::cout << "Please type in your full name and age " << std::endl;
 
-    std::getline(std::cin,full_name);
+    std::getline(std::cin, full_name);
 
     std::cin >> age3;
 
-    std::cout << "Hello " << full_name 
-            << " you are " << age3 << " years old!" << std::endl;
+    std::cout << "Hello " << full_name << " you are " << age3 << " years old!" << std::endl;
 
-
- 
-  return 0;
+    return 0;
 }
---
+++ 3.FirstSteps/Assignment_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string country;
+    std::cout << "Where do you live?" << std::endl;
+    // For single word country
+    // std::cin >> country;
+    // For countries with more than one word e.g. South Africa
+    std::getline(std::cin, country);
+    std::cout << "I've heard great things about " << country << ". I'd like to go sometime.";
+
+    return 0;
+}
