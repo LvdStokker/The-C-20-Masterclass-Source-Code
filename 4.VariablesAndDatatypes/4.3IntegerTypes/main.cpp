cb7b3e70e98a79e69f13282fb02a83ee490d9e1f
Luke van der Stokker <lukevanderstokker@gmail.com>
2023-08-06 14:40:39 +0200

    integer types refactoring and notes

--- 4.VariablesAndDatatypes/4.3IntegerTypes/main.cpp
+++ 4.VariablesAndDatatypes/4.3IntegerTypes/main.cpp
@@ -1,57 +1,49 @@
 #include <iostream>
 
-
-int main(){
-
-    //Braced initializers
+int main()
+{
+    // Braced initializers
     /*
-    //Variable may contain random garbage value . WARNING
+    // Variable may contain random garbage value . WARNING
     int elephant_count;
-    
-    int lion_count{};//Initializes to zero
-    
-    int dog_count {10}; //Initializes to 10
-    
-    int cat_count {15}; //Initializes to 15
-    
-    //Can use expression as initializer
-    int domesticated_animals { dog_count + cat_count };
 
-    //
-    //int new_number{doesnt_exist};
+    int lion_count{}; // Initializes to zero
 
-   // int narrowing_conversion {2.9};//Compiler error
+    int dog_count{10}; // Initializes to 10
+
+    int cat_count{15}; // Initializes to 15
+
+    // Can use expression as initializer
+    int domesticated_animals{dog_count + cat_count};
+
+    // int new_number{doesnt_exist}; // Cannot initialise to unknown var
+
+    // int narrowing_conversion {2.9}; // Compiler error
 
     std::cout << "Elephant count : " << elephant_count << std::endl;
     std::cout << "Lion count : " << lion_count << std::endl;
     std::cout << "Dog count : " << dog_count << std::endl;
     std::cout << "Cat count : " << cat_count << std::endl;
     std::cout << "Domesticated animal count : " << domesticated_animals << std::endl;
-   */
+    */
 
-
-
-
-   //Functional Initialization
-   /*
+    // Functional Initialization
+    /*
     int apple_count(5);
     int orange_count(10);
-    int fruit_count (apple_count + orange_count);
-    //int bad_initialization ( doesnt_exist3 + doesnt_exist4 );
+    int fruit_count(apple_count + orange_count);
+    // int bad_initialization ( doesnt_exist3 + doesnt_exist4 );
 
-    //Information lost. less safe than braced initializers
-    int narrowing_conversion_functional (2.9);
-    
-    
+    // Information lost. less safe than braced initializers
+    int narrowing_conversion_functional(2.9);
+
     std::cout << "Apple count : " << apple_count << std::endl;
     std::cout << "Orange count : " << orange_count << std::endl;
     std::cout << "Fruit count : " << fruit_count << std::endl;
-    std::cout << "Narrowing conversion : " << narrowing_conversion_functional << std::endl;//Will loose info
+    std::cout << "Narrowing conversion : " << narrowing_conversion_functional << std::endl; // Will loose info
     */
+    // Assignment notation
 
-
-   //Assignment notation
-    
     int bike_count = 2;
     int truck_count = 7;
     int vehicle_count = bike_count +truck_count;
@@ -61,7 +53,6 @@ int main(){
     std::cout << "Truck count : " << truck_count << std::endl;
     std::cout << "Vehicle count : " << vehicle_count << std::endl;
     std::cout << "Narrowing conversion : " << narrowing_conversion_assignment << std::endl;
-    
 
     //Check the size with sizeof
     std::cout << "sizeof int : " << sizeof(int) << std::endl;
