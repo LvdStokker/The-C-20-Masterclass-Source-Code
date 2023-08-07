6cae4737c9a65c175be240e106989e53359e5b86
Luke van der Stokker <lukevanderstokker@gmail.com>
2023-08-07 12:20:05 +0200

    Refactor and notes about in-place

--- 5.OperationsOnData/5.5.CompoundAssignmentOperators/main.cpp
+++ 5.OperationsOnData/5.5.CompoundAssignmentOperators/main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
-int main(){
-    
-	int value {45};
-    
+int main()
+{
+    int value{45};
+
     std::cout << "The value is : " << value << std::endl;
-    
+
     std::cout << std::endl;
     value = value + 5;
-    //value +=5; // equivalent to value = value + 5
+    // value +=5; // equivalent to value = value + 5
     std::cout << "The value is (after +=5) : " << value << std::endl; // 50
 
     std::cout << std::endl;
-    value -=5; // equivalent to value = value - 5
-    std::cout << "The value is (after -=5) : " << value << std::endl; // 45 
+    value -= 5;                                                       // equivalent to value = value - 5
+    std::cout << "The value is (after -=5) : " << value << std::endl; // 45
 
     std::cout << std::endl;
-    value *=2;
+    value *= 2;
     std::cout << "The value is (after *=2) : " << value << std::endl; // 90
 
     std::cout << std::endl;
@@ -25,6 +25,9 @@ int main(){
 
     std::cout << std::endl;
     value %= 11;
-    std::cout << "The value is (after %=11) : " << value << std::endl;// 8
+    std::cout << "The value is (after %=11) : " << value << std::endl; // 8
+
+    // Can do compound operation in-place but here we need parentheses
+    std::cout << "The value is (after +=2 in-place) : " << (value+=2) << std::endl; // 10
     return 0;
 }
