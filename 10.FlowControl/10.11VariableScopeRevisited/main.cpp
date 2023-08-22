d0fd163384fb74b501dd53a244a7d5c05e68f54e
Luke van der Stokker <lukevanderstokker@gmail.com>
2023-08-22 22:36:08 +0200

    refactor and some more comments on scope

--- 10.FlowControl/10.11VariableScopeRevisited/main.cpp
+++ 10.FlowControl/10.11VariableScopeRevisited/main.cpp
@@ -2,24 +2,27 @@
 
 int global_var{44};
 
-int main(){
-
-    bool green {false};
+int main()
+{
+    bool green{false};
 
     global_var++; // 45
 
-    if(green){
-        global_var++; 
-        int car_count{23};
+    if (green)// if we init car_count here then it will also be visible in the else condition
+    {
+        global_var++;
+        int car_count{23}; // Only exists in this scope
         std::cout << "Light is green " << car_count << "on the move!" << std::endl;
-    }else{
-        //car_count++; // Compiler error
+    }
+    else
+    {
+        // car_count++; // Compiler error
         global_var++; // 46
         std::cout << "Light is not green. Y'all should STOP!" << std::endl;
     }
 
-    //car_count++;
+    // car_count++;
     std::cout << "global_var : " << global_var << std::endl;
-   
+
     return 0;
 }
